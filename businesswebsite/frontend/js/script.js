f16b10a3f51f6a00d09019dd4c0c5e95c52b167c
Erick Gonzalez <erickygonzalez@outlook.com>
2025-02-19 21:59:34 -0600

    Moved nav bar dropdown in-line script to script.js to be reusable on all pages

--- businesswebsite/frontend/js/script.js
+++ businesswebsite/frontend/js/script.js
@@ -76,3 +76,10 @@ document.getElementById("contact-form").addEventListener("submit", function(even
     });
 });
 
+//Drop down functionality for nav bar.
+const dropdownBtn = document.querySelector('.dropdown-btn');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+dropdownBtn.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('active');
+});
